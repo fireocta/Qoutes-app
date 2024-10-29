399663e439c17acb7e7c9862336f3ea0ed1b2b49
octa <mailoctacupcake@gmail.com>
2024-10-29 23:24:50 +0530

    Added a Button

--- app/src/main/java/com/octacupcake/qoutes/MainActivity.kt
+++ app/src/main/java/com/octacupcake/qoutes/MainActivity.kt
@@ -3,6 +3,14 @@ package com.octacupcake.qoutes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.octacupcake.qoutes.ui.theme.QoutesTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,9 +18,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             QoutesTheme {
-
-
+                    QuotesApp()
             }
         }
     }
 }
+@Preview
+@Composable
+fun QuotesApp() {
+    Quotes(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    )
+}
+
+@Composable
+fun Quotes(modifier: Modifier = Modifier){
+
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Button(onClick = { /*TODO*/ }) {
+
+        }
+    }
+
+}
+
