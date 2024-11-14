efab08631ba727bf223151abf37fce94970cd7d5
octa <mailoctacupcake@gmail.com>
2024-11-14 17:22:38 +0530

    Button with Previous text added

--- app/src/main/java/com/octacupcake/qoutes/MainActivity.kt
+++ app/src/main/java/com/octacupcake/qoutes/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.octacupcake.qoutes.ui.theme.QoutesTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,13 +40,26 @@ fun QuotesApp() {
 
 @Composable
 fun Quotes(modifier: Modifier = Modifier){
-
     Column (
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-        Button(onClick = { /*TODO*/ }) {
+        Row {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.width(120.dp)
+            ){
+                Text(text="Previous")
 
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.width(120.dp)
+            ) {
+                Text(text="Next")
+
+            }
         }
     }
 
